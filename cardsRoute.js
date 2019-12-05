ff67851a71b61c64829aacf1318c1596f1431a20
Matthew <matthewdkemp91@gmail.com>
2019-12-05 16:06:50 +0000

    Added countCards function and get request to send number of each category a users has got

--- cardsRoute.js
+++ cardsRoute.js
@@ -44,69 +44,48 @@ router.post("/new", (req, res) => {
     var senddate = req.body.senddate;
     var message = req.body.message;
     var picurl = req.body.picurl;
-    var p=0;
-    var r=0;
-    var i=0;
-    var d=0;
-    var e=0;
+
     con.query(`insert into cards values('${rempno}', '${rmempno}',
-                '${sempno}', '${category}', '${senddate}', '${message}', '${picurl}')`, (err, result) => {
-            if (err) {
-                throw err;
-            }
-        })
-
-    con.query(`select count(rempno) from cards where category='P' and rempno='${rempno}'`,function(error,result){
-        result.forEach(element => {
-            p=element['count(rempno)'];
-        });
-    });
-
-    con.query(`select count(rempno) from cards where category='R' and rempno='${rempno}'`,function(error,result){
-        result.forEach(element => {
-            r=element['count(rempno)'];
-        });
-    });
-
-    con.query(`select count(rempno) from cards where category='I' and rempno='${rempno}'`,function(error,result){
-        result.forEach(element => {
-            i=element['count(rempno)'];
-        });
-    });
-
-    con.query(`select count(rempno) from cards where category='D' and rempno='${rempno}'`,function(error,result){
-        result.forEach(element => {
-            d=element['count(rempno)'];
-        });
-    });
-
-    con.query(`select count(rempno) from cards where category='E' and rempno='${rempno}'`,function(error,result){
-        result.forEach(element => {
-            e=element['count(rempno)'];
-        });
-    });
-
-    console.log("Number of P: "+p);
-    console.log("Number of R: "+r);
-    console.log("Number of I: "+i);
-    console.log("Number of D: "+d);
-    console.log("Number of E: "+e);
-        
+    '${sempno}', '${category}', '${senddate}', '${message}', '${picurl}')`, (err, result) => {
+        if (err) {
+            throw err;
+        } else{
+            
+        }
+    })
+       
     res.send("Pride card submitted");
-    
 })
 
-// router.get("/cards/home", (req, res) => {
-//     con.query(`SELECT * FROM cards WHERE YEAR(senddate) = YEAR(CURRENT_DATE)
-//      AND MONTH(senddate) = MONTH(CURRENT_DATE)`, (err, result, fields) => {
-//         console.log(result);
-//         if (err) {
-//             throw err;
-//         }
-//         else {
-//             res.send(result);
-//         }
-//     })
-// })
+router.get("/cardNumbers", (req, res) => {
+    var rempno = 'P04967'; //Needs to be obtained via login session object
+
+    async function getValues(){
+        var p= await countCards('P',rempno);
+        var r= await countCards('R',rempno);
+        var i= await countCards('I',rempno);
+        var d= await countCards('D',rempno);
+        var e= await countCards('E',rempno);
+        var values={"P":p,"R":r,"I":i,"D":d,"E":e}
+        res.send(values);
+    }
+
+    getValues();
+
+})
+
+function countCards(cardCategory,rempno){
+    return new Promise(function (resolve, reject) {
+        con.query(`select count(rempno) as cardCount from cards where category='${cardCategory}' and rempno='${rempno}'`, function(error,result){
+            if(result){
+                let res= result[0].cardCount;
+                resolve(res);
+            }
+            else{
+                reject("Error");
+            }
+        });
+    })
+}
 
 module.exports = router
